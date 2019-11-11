412418556a2b460ddf3cf50f5fff03ef63c4b91f
DJTrumpDaddy <42254304+DJTrumpDaddy@users.noreply.github.com>
2019-11-11 16:27:57 -0600

    edited formatting

--- Simple16BitDivision.asm
+++ Simple16BitDivision.asm
@@ -20,10 +20,10 @@
 div16:
 	SUB 	numL,denL				;subtract lower denominator byte from lower numerator byte
 	SBC 	numH,denH				;subtract (higher denominator byte + C) from higher numerator byte
-	BRCS	div16bit_remainder		;if numerator higher byte requires a carry, calculate remainder
-	INC		result					;increment result
+	BRCS	div16bit_remainder			;if numerator higher byte requires a carry, calculate remainder
+	INC	result					;increment result
 	RJMP	div16bit				;loop back
 div16_remainder:
 	MOV		remainder,numL			;copy lower byte of numerator into remainder
 	ADD		remainder,denL			;add lower byte of denominator into remainder (throws a carry)
-	RET								;return
+	RET						;return
